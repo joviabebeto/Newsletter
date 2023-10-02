539ab44cacf6d05f6396d05e3af427706862301b
joviabebeto <brigittetuyishimire5@gmail.com>
2023-10-02 13:33:40 +0200

    push

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import router from './routes/index';
 import bodyParser from 'body-parser';
+import cors from "cors"
 
 
 const app=express();
 dotenv.config()
 app.use(bodyParser.json())
+app.use(cors())
 app.use("/api/v1",router)
 const database=process.env.DATABASE
 const port=process.env.PORT
